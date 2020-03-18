7e66c169fcf6805ef657b3420d3504b2007d72a8
Illia Kuzmenko <ik@Illias-MacBook-Pro.local>
2020-03-18 21:07:54 +0200

    add server part

---
+++ serverPart/bme_to_googlesheets/bme_to_googlesheets.ino
@@ -0,0 +1,121 @@
+#include <WiFi.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+// Replace with your SSID and Password
+const char* ssid     = "********";
+const char* password = "********";
+
+// Replace with your unique IFTTT URL resource
+const char* resource = "/trigger/bme280_readings/with/key/lH9U7jkzQj08O9XLsyQm2rLeZRBx7gDn2l-vKZ_rJap";
+
+// Maker Webhooks IFTTT
+const char* server = "maker.ifttt.com";
+
+// Time to sleep
+uint64_t uS_TO_S_FACTOR = 1000000;  // Conversion factor for micro seconds to seconds
+uint64_t TIME_TO_SLEEP = 3600;      // Sleep for 60 minutes = 3600 seconds
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C init
+
+void setup() {
+  Serial.begin(115200); 
+  delay(2000);
+
+  // initialize BME280 sensor
+  bool status;
+  status = bme.begin(0x76);  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  initWifi();
+  makeIFTTTRequest();
+    
+  #ifdef ESP32
+    // enable timer deep sleep
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);    
+    Serial.println("Going to sleep now");
+    // start deep sleep for 3600 seconds (60 minutes)
+    esp_deep_sleep_start();
+  #else
+    // Deep sleep mode for 3600 seconds (60 minutes)
+    Serial.println("Going to sleep now");
+    ESP.deepSleep(TIME_TO_SLEEP * uS_TO_S_FACTOR); 
+  #endif
+}
+
+void loop() {
+  // sleeping so wont get here 
+}
+
+// Establish a Wi-Fi connection with your router
+void initWifi() {
+  Serial.print("Connecting to: "); 
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);  
+
+  int timeout = 10 * 4; // 10 seconds
+  while(WiFi.status() != WL_CONNECTED  && (timeout-- > 0)) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  if(WiFi.status() != WL_CONNECTED) {
+     Serial.println("Failed to connect, going back to sleep");
+  }
+
+  Serial.print("WiFi connected in: "); 
+  Serial.print(millis());
+  Serial.print(", IP address: "); 
+  Serial.println(WiFi.localIP());
+}
+
+// Make an HTTP request to the IFTTT web service
+void makeIFTTTRequest() {
+  Serial.print("Connecting to "); 
+  Serial.print(server);
+  
+  WiFiClient client;
+  int retries = 5;
+  while(!!!client.connect(server, 80) && (retries-- > 0)) {
+    Serial.print(".");
+  }
+  Serial.println();
+  if(!!!client.connected()) {
+    Serial.println("Failed to connect...");
+  }
+  
+  Serial.print("Request resource: "); 
+  Serial.println(resource);
+
+  // Temperature in Celsius
+  String jsonObject = String("{\"value1\":\"") + bme.readTemperature() + "\",\"value2\":\"" + (bme.readPressure()/100.0F)
+                      + "\",\"value3\":\"" + bme.readHumidity() + "\"}";   
+  client.println(String("POST ") + resource + " HTTP/1.1");
+  client.println(String("Host: ") + server); 
+  client.println("Connection: close\r\nContent-Type: application/json");
+  client.print("Content-Length: ");
+  client.println(jsonObject.length());
+  client.println();
+  client.println(jsonObject);
+        
+  int timeout = 5 * 10; // 5 seconds             
+  while(!!!client.available() && (timeout-- > 0)){
+    delay(100);
+  }
+  if(!!!client.available()) {
+    Serial.println("No response...");
+  }
+  while(client.available()){
+    Serial.write(client.read());
+  }
+  
+  Serial.println("\nclosing connection");
+  client.stop(); 
+}
