b7bd7aed66f23d1c1e853b35e40344809dc9abed
Illia Kuzmenko <ik@Illias-MacBook-Pro.local>
2020-03-18 23:17:39 +0200

    add progress bar and colorama

--- clientPart/main.py
+++ clientPart/main.py
@@ -1,9 +1,30 @@
-from pprint import pprint
-
+import os
+import time
+from progress.bar import IncrementalBar
+import pyowm
 import httplib2
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
 
+# Progress bar 
+bar = IncrementalBar('Processing', max = 10)
+progressBar = [i for i in range(10)]
+for item in progressBar[:1]:
+    bar.next()
+    time.sleep(0.05)
+
+
+# Ініціалізую API openweathermap.org
+owm = pyowm.OWM('6d00d1d4e704068d70191bad2673e0cc', language = "ua")
+observation = owm.weather_at_place('Бориспіль')
+w = observation.get_weather()
+# Ініціалізую необхідні змінні з даними тиску, вологості, температури
+press = w.get_pressure()["press"]
+humi = w.get_humidity()
+temp = w.get_temperature('celsius')["temp"]
 
 # Файл Google Developer Console
 CREDENTIALS_FILE = 'creds.json'
@@ -18,15 +39,67 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(
 httpAuth = credentials.authorize(httplib2.Http())
 service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
 
+######################################TEMPERATURE###########################################
+# Чтання файлу google sheets стовпчик TEMPERATURE
+valuesTemperature = service.spreadsheets().values().get(
+    spreadsheetId=spreadsheet_id,
+    range='B2:B',
+    majorDimension='ROWS'
+).execute()
+# Створив список "temperatureList" і записав у неї значення з словника valuesTemperature за ключем 'values'
+temperatureList = valuesTemperature.get('values')
+
+# Створив список в який запишу дані float (дані телеметрії: атмосферний тиск)
+temperatureFloatList = []
+
+# Записав дані типу float в новий список
+for item in temperatureList:
+    temperatureFloatList.append(float(item[0]))
+
+# Роблю реверс щоб підняти актуальні данні до верху списку
+TemperatureList = list(reversed(temperatureFloatList))
+
+# Progress bar 
+for item in progressBar[1:4]:
+    bar.next()
+    time.sleep(0.05)
+
+
+######################################HUMIDITY##############################################
+# Чтання файлу google sheets стовпчик HUMIDITY
+valuesHumidity = service.spreadsheets().values().get(
+    spreadsheetId=spreadsheet_id,
+    range='D2:D',
+    majorDimension='ROWS'
+).execute()
+# Створив список "humidityList" і записав у неї значення з словника valuesHumidity за ключем 'values'
+humidityList = valuesHumidity.get('values')
+
+# Створив список в який запишу дані float (дані телеметрії: атмосферний тиск)
+humidityFloatList = []
+
+# Записав дані типу float в новий список
+for item in humidityList:
+    humidityFloatList.append(float(item[0]))
+
+# # Роблю реверс щоб підняти актуальні данні до верху списку
+HumidityList = list(reversed(humidityFloatList))
+
+# Progress bar 
+for item in progressBar[4:7]:
+    bar.next()
+    time.sleep(0.05)
+
+######################################PRESURE###############################################
 # Чтання файлу google sheets стовпчик PRESURE
-values = service.spreadsheets().values().get(
+valuesPressure = service.spreadsheets().values().get(
     spreadsheetId=spreadsheet_id,
     range='C2:C',
     majorDimension='ROWS'
 ).execute()
 
-# Створив список "presureList" і записав у неї значення з словника "values" за ключем 'values'
-pressureList = values.get('values')
+# Створив список "presureList" і записав у неї значення з словника valuesPressure за ключем 'values'
+pressureList = valuesPressure.get('values')
 
 # Створив список в який запишу дані float (дані телеметрії: атмосферний тиск)
 pressureFloatList = []
@@ -35,12 +108,21 @@ pressureFloatList = []
 for item in pressureList:
     pressureFloatList.append(float(item[0]))
 
-# Роблю реверс списку і рахую кількість елементів масиву щоб відфільтрувати неактуальні дані
-revPressureList = list(reversed(pressureFloatList))
-lenList = len(revPressureList)
+# # Роблю реверс щоб підняти актуальні данні до верху списку і рахую кількість елементів масиву щоб відфільтрувати неактуальні дані
+PressureList = list(reversed(pressureFloatList))
+lenList = len(PressureList)
+
+# Progress bar
+for item in progressBar[7:10]:
+    bar.next()
+    time.sleep(0.05) 
+bar.finish()
+os.system('clear')
+
+############################################################################################
 
 # Створюю змінну для актуальних даних
-oneHourPressure = revPressureList
+oneHourPressure = PressureList
 # Прибираю неактуальні дані зі списку
 while lenList > 24 :
 	oneHourPressure.pop()
@@ -74,17 +156,24 @@ while lenThreeHoursPressure > 8 :
 #for item in threeHoursPressure:
 #	print(item)
 
+
+
+print()
+print(Fore.YELLOW + "За даними ресурсу openweathermap.org:")
+print("В місті Бориспіль зараз " + w.get_detailed_status())
+print("Температура – " + str(temp) + "C°")
+print("Вологість – " + str(humi) + "%")
+print("Тиск – " + str(press) + "hPa" + Style.RESET_ALL)
+print()
+print(Fore.GREEN + "За даними зібраними за допомогою датчика BME280:")
+print("В місті Бориспіль зараз " + w.get_detailed_status())
+print("Температура – " + str(TemperatureList[0]) + "C°")
+print("Вологість – " + str(HumidityList[0]) + "%")
+print("Тиск – " + str(PressureList[0]) + "hPa" + Style.RESET_ALL)
+print()
 now = threeHoursPressure[0]
 threeHoursAgo = threeHoursPressure[1]
-
-#print()
-print("Теперішні показники тиску: " + str(now) + "hPa")
-print("Попередні показники тиску: " + str(threeHoursAgo) + "hPa")
-print()
-
 difference = now - threeHoursAgo
-print("Різниця між показниками за останні 3 години складає: " + str("%.2f" % difference) + "hPa")
-print()
 
 if difference < 1.1 and difference > -1.1 :
 	print("Різких змін погодних умов не прогнозується.")
@@ -106,3 +195,11 @@ elif difference < -10 :
 	print("Шторм, буря.")
 else:
 	print("error")
+ 
+#print("Попередні показники тиску: " + str(threeHoursAgo) + "hPa")
+#print("Теперішні показники тиску: " + str(now) + "hPa")
+print("Різниця між показниками тиску за останні 3 години складає: " + str("%.2f" % difference) + "hPa")
+print()
+
+
+
